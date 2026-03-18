b0d763005ff17efba060881c09b58dcc966c0567
Allison Smith <smith14817@gmail.com>
2026-03-18 13:50:23 -0400

    Merge UI branch and clean project structure
    - Cleaned up UI components and layout
    - Reorganized project file structure
    - Integrated database functionality into MainWindow
    - Removed unnecessary files and ensured proper .gitignore usage

---
+++ FindlayBikeShop/BikeDetails.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+
+
+namespace FindlayBikeShop
+{
+    /// <summary>
+    /// Interaction logic for BikeDetails.xaml
+    /// </summary>
+    public partial class BikeDetails : Window
+    {
+        public BikeDetails(Bike bike)
+        {
+            InitializeComponent();
+
+            this.DataContext = bike;
+        }
+
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Inventory_Click(object sender, RoutedEventArgs e)
+        {
+            var inventoryWindow = new Inventory();
+            inventoryWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            var maintenanceWindow = new MaintenanceHistory();
+            maintenanceWindow.Show();
+            this.Close();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
